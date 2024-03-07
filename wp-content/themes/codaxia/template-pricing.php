b57254efbe57589d301dfefbb8f035a0e3cb3f99
Abdelhadi Mejdoubi <abdelhadi.mejdoubi.1@gmail.com>
2024-03-07 12:28:13 -0500

    pricing page with html

--- wp-content/themes/codaxia/template-pricing.php
+++ wp-content/themes/codaxia/template-pricing.php
@@ -5,14 +5,15 @@ Template Name: Pricing
 ?>
 <?php get_header()?>
 
-<section id="pricing-plans" class="pt-120 pb-100 text-light">
+<section id="pricing-plans" class="pt-120 pb-100 text-light pricing">
 	<div class="container">
 		<!-- Title and Description -->
 		<div class="row ">
 			<div class="col-lg-12">
-				<div class="section-title text-center mb-60">
-					<h1 class="mb-20 wow fadeInUp" data-wow-delay=".2s">Pricing Plans For Everyone</h1>
-					<p class="wow fadeInUp" data-wow-delay=".4s">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                <div class="section-title text-center mb-60">
+					<h1 class="mb-20 wow fadeInUp" data-wow-delay=".2s">Création de site web personalisé clé en main</h1>
+					<p class="wow fadeInUp" data-wow-delay=".4s">Nous prennons en main la conception du site web du design jusqu'a la mise en ligne puis la maintenance.</p>
+                    <p class="wow fadeInUp" data-wow-delay=".4s">Notre offre met l'accent sur la personalisation et l'evolutivité pour un résultat adapté au besoin de votre entreprise( visibilité, trouver de nouveau clients, informer sur un produit ...) </p>
 				</div>
 			</div>
 		</div>
@@ -33,86 +34,105 @@ Template Name: Pricing
 		<div class="row">
 			<!-- Starter Plan -->
 			<div class="col-lg-3 col-md-6 mb-4">
-				<div class="card bg-dark text-white rounded h-100">
-					<div class="card-header">
-						<h4 class="text-primary">Single page website</h4>
-						<p>Great for personal websites and projects</p>
-					</div>
-					<div class="card-body d-flex flex-column justify-content-between">
-						<h3 class="text-success">2000<span>cad +tx</span></h3>
-						<ul class="list-unstyled text-start">
-							<li><i class="lni lni-checkmark-circle"></i> 1 Page</li>
-							<li><i class="lni lni-checkmark-circle"></i> Mobile Ready Design</li>
-							<li><i class="lni lni-checkmark-circle"></i> 3 to 6 sections * </li>
-							<li><i class="lni lni-checkmark-circle"></i> up to 2 features* </li>
-							<li><i class="lni lni-checkmark-circle"></i> 1 Year FREE Hosting!</li>
-						</ul>
-					</div>
-					<div class="card-footer bg-transparent">
-						<a href="#" class="btn btn-success btn-block">BUY NOW</a>
-					</div>
+				<div class="single-bloc-pricing text-white h-100">
+                    <div class="subtitle">
+                        <div class="row card-header icon">
+                            <i class="lni lni-diamond-alt col-lg-1"></i>
+                            <h4 class="col-lg-1">Basic</h4>
+                            <p class="text-white">Personal websites</p>
+                        </div>
+                        <div class="card-body d-flex flex-column justify-content-between">
+                            <p>starting from</p>
+                            <h3 class="text-white">$2000</h3>
+                            <ul class="list-unstyled text-start subtitle">
+                                    <li><i class="lni lni-checkmark-circle"></i> 1 Page</li>
+                                    <li><i class="lni lni-checkmark-circle"></i> Mobile Ready Design</li>
+                                    <li><i class="lni lni-checkmark-circle "></i> 3 to 6 sections* </li>
+                                    <li><i class="lni lni-checkmark-circle"></i> Up to 2 features* </li>
+                                    <li><i class="lni lni-checkmark-circle"></i> 1 Year FREE Hosting!</li>  
+                            </ul>
+                        </div>
+                        <div class="card-footer bg-transparent">
+                            <a href="#" class="btn button-buy btn-block fw-bold">BUY NOW</a>
+                        </div>
+                    </div>
 				</div>
 			</div>
 			<!-- Premium Plan -->
 			<div class="col-lg-3 col-md-6 mb-4">
-				<div class="card bg-dark text-white rounded h-100">
-					<div class="card-header">
-						<h4 class="text-primary">Company website</h4>
-						<p>For small businesses.</p>
-					</div>
-					<div class="card-body d-flex flex-column justify-content-between">
-						<h3 class="text-success">3000<span>cad + tx</span></h3>
-						<ul class="list-unstyled text-start">
-							<li><i class="lni lni-checkmark-circle"></i> 3 Page</li>
-							<li><i class="lni lni-checkmark-circle"></i> Mobile Ready Design</li>
-							<li><i class="lni lni-checkmark-circle"></i> up to 4 features* </li>
-							<li><i class="lni lni-checkmark-circle"></i> 1 Year FREE Hosting!</li>
-						</ul>
-					</div>
-					<div class="card-footer bg-transparent">
-						<a href="#" class="btn btn-success btn-block">BUY NOW</a>
-					</div>
+				<div class="single-bloc-pricing text-white h-100">
+                    <div class="subtitle">
+                        <div class="recommanded">
+                            <p class="text-white">Recommanded</p>
+                        </div>
+                        <div class="row card-header icon">
+                            <i class="lni lni-bar-chart"></i>
+                            <h4 class="col-lg-1">Pro</h4>
+                            <p class="text-white">Small businesses</p>
+                        </div>
+                        <div class="card-body d-flex flex-column justify-content-between">
+                            <p>starting from</p>
+                            <h3 class="text-white">$3000</h3>
+                            <ul class="list-unstyled text-start subtitle">
+                                    <li><i class="lni lni-checkmark-circle"></i> 3 Pages</li>
+                                    <li><i class="lni lni-checkmark-circle"></i> Mobile Ready Design</li>
+                                    <li><i class="lni lni-checkmark-circle "></i> Up to 4 features* </li>
+                                    <li><i class="lni lni-checkmark-circle"></i> 1 Year FREE Hosting!</li>  
+                            </ul>
+                        </div>
+                        <div class="card-footer bg-transparent">
+                            <a href="#" class="btn button-buy btn-block fw-bold">BUY NOW</a>
+                        </div>
+                    </div>
 				</div>
 			</div>
 			<!-- Ultimate Plan -->
 			<div class="col-lg-3 col-md-6 mb-4">
-				<div class="card bg-dark text-white rounded h-100">
-					<div class="card-header">
-						<h4 class="text-primary">Ultimate</h4>
-						<p>For large scale enterprises.</p>
-					</div>
-					<div class="card-body d-flex flex-column justify-content-between">
-						<h3 class="text-success">4500<span>cad + tx</span></h3>
-						<ul class="list-unstyled text-start">
-							<li><i class="lni lni-checkmark-circle"></i> 5 Page</li>
-							<li><i class="lni lni-checkmark-circle"></i> Mobile Ready Design</li>
-							<li><i class="lni lni-checkmark-circle"></i> up to 6 features* </li>
-							<li><i class="lni lni-checkmark-circle"></i> 1 Year FREE Hosting!</li>
-						</ul>
-					</div>
-					<div class="card-footer bg-transparent">
-						<a href="#" class="btn btn-success btn-block">BUY NOW</a>
-					</div>
+				<div class="single-bloc-pricing text-white h-100">
+                    <div class="subtitle">
+                        <div class="row card-header icon">
+                            <i class="lni lni-dropbox"></i>
+                            <h4 class="col-lg-1">Premium</h4>
+                            <p class="text-white">Large scale enterprises</p>
+                        </div>
+                        <div class="card-body d-flex flex-column justify-content-between">
+                            <p>starting from</p>
+                            <h3 class="text-white">$4500</h3>
+                            <ul class="list-unstyled text-start subtitle">
+                                    <li><i class="lni lni-checkmark-circle"></i> 5 Pages</li>
+                                    <li><i class="lni lni-checkmark-circle"></i> Mobile Ready Design</li>
+                                    <li><i class="lni lni-checkmark-circle"></i> Up to 6 features* </li>
+                                    <li><i class="lni lni-checkmark-circle"></i> 1 Year FREE Hosting!</li>
+                            </ul>
+                        </div>
+                        <div class="card-footer bg-transparent">
+                            <a href="#" class="btn button-buy btn-block fw-bold">BUY NOW</a>
+                        </div>
+                    </div>
 				</div>
 			</div>
 			<!-- Custom Plan -->
 			<div class="col-lg-3 col-md-6 mb-4">
-				<div class="card bg-dark text-white rounded h-100">
-					<div class="card-header">
-						<h4 class="text-primary">Custom website</h4>
-						<p>For large scale enterprises.</p>
-					</div>
-					<div class="card-body d-flex flex-column justify-content-between">
-						<h3 class="text-success">4500<span>cad + tx</span></h3>
-						<ul class="list-unstyled text-start">
-							<li><i class="lni lni-checkmark-circle"></i> unlimited Page</li>
-							<li><i class="lni lni-checkmark-circle"></i> Mobile Ready Design</li>
-							<li><i class="lni lni-checkmark-circle"></i> Custom features </li>
-						</ul>
-					</div>
-					<div class="card-footer bg-transparent">
-						<a href="#" class="btn btn-success btn-block">BUY NOW</a>
-					</div>
+				<div class="single-bloc-pricing text-white h-100">
+                    <div class="subtitle">
+                        <div class="row card-header icon">
+                        <i class="lni lni-stats-up"></i>
+                            <h4 class="col-lg-1">Ultimate</h4>
+                            <p class="text-white">Large scale enterprises</p>
+                        </div>
+                        <div class="card-body d-flex flex-column justify-content-between">
+                            <p>starting from</p>
+                            <h3 class="text-white">$4500</h3>
+                            <ul class="list-unstyled text-start subtitle">
+                                    <li><i class="lni lni-checkmark-circle"></i> Unlimited Page</li>
+                                    <li><i class="lni lni-checkmark-circle"></i> Mobile Ready Design</li>
+                                    <li><i class="lni lni-checkmark-circle"></i> Custom features </li>  
+                            </ul>
+                        </div>
+                        <div class="card-footer bg-transparent">
+                            <a href="#" class="btn button-buy btn-block fw-bold">BUY NOW</a>
+                        </div>
+                    </div>
 				</div>
 			</div>
 		</div>
