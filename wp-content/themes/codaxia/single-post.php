4a65e1a77eebe01cf97a3979742466dd87f97545
Abdelhadi Mejdoubi <abdelhadi.mejdoubi.1@gmail.com>
2024-03-08 14:55:33 -0500

    adjusted the mobile format in blog section

--- wp-content/themes/codaxia/single-post.php
+++ wp-content/themes/codaxia/single-post.php
@@ -4,16 +4,17 @@
 	<section id="post" class="hero-section blog-page">
 		<div class="container">
 			<div class="row justify-content-center">
-				<div class="col-lg-8 col-md-12 col-12 mb-2">
-					<div class="row single-bloc-blog single-blog col-lg-14 p-4 mb-4 mr-n5">
+				<div class="col-lg-9 col-md-12 col-12 mb-2">
+					<div class="row single-bloc-blog single-blog col-lg-12 p-4 mb-4 mr-n5">
 						<div class="content col-lg-6">
 							<h1 class="text-dark text-center"> <?php the_title() ?> </h1>
 							</br>
-							<div class="text-muted fs-7">
+							<div class="text-dark fs-7">
 								<p><?php the_excerpt() ?></p>
 								</br>
 								<p> By: <?= get_field('autor_name'); ?> </p>
 								<p><?php echo get_the_date(); ?></p>
+								</br>
 							</div>
 						</div>
 						<div class="image-article col-lg-6" style="background-image: url('<?php echo esc_url(get_the_post_thumbnail_url()); ?>');">
@@ -25,10 +26,10 @@
 					<hr>
 					</div>
 					<div class="row">
-						<div class="image-autor col-lg-1">
+						<div class="image-autor col-lg-1 col-sm-2">
 							<img src="<?= get_field('author_image'); ?>" alt="">
 						</div>
-						<div class="col-lg-2 fs-5 ms-5">
+						<div class="col-lg-2 col-4 fs-4 ms-4">
 							<div>
 								<p class="text-white fw-bolder"> <?= get_field('author_name'); ?> </p>
 								<p class="mb-1 text-white"> <?= get_field('role_author'); ?> </p>
